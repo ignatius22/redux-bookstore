f5654432a164c082295ac85608f5a0b1c82dec0b
jstloyal <jstloyalty@gmail.com>
2020-11-10 22:16:12 +0100

    component storing the title and book category in its own state

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createBook } from '../actions/index';
 
-const BookForm = () => {
-  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  return (
-    <form>
-      <label htmlFor="title">
-        Title
-        <input id="title" type="text" name="title" />
-      </label>
-      <select id="category">
-        {categories.map(e => <option key={e} value={e}>{e}</option>)}
-      </select>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+class BookForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: categories[0],
+    };
+    this.baseState = this.state;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+}
+
+// const BookForm = () => {
+//   const categories = [
+//     'Action',
+//     'Biography',
+//     'History',
+//     'Horror',
+//     'Kids',
+//     'Learning',
+//     'Sci-Fi'
+//   ];
+//   return (
+//     <form>
+//       <label htmlFor="title">
+//         Title
+//         <input id="title" type="text" name="title" />
+//       </label>
+//       <select id="category">
+//         {categories.map(e => <option key={e} value={e}>{e}</option>)}
+//       </select>
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// };
 
 export default BookForm;
