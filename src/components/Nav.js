142cbea9ccb036e01d096cb99d1d7541181402e1
jstloyal <jstloyalty@gmail.com>
2020-11-12 07:56:51 +0100

    add style to navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import userImg from '../user.svg';
 
 const Nav = () => {
   return (
-    <div>
-      <h1>Bookstore CMS</h1>
+    <div className="navbar">
+      <h1 className="app-logo">Bookstore CMS</h1>
       <p>BOOKS</p>
-      <p>CATEGORIES</p>
+      <p className="disabled">CATEGORIES</p>
+      <div className="user-img">
+        <img src={userImg} alt="profile" />
+      </div>
     </div>
   );
 };
